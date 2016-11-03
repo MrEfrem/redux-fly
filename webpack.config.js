ab769bc52ba18fed6d066e986aece7d96e980c90
MrEfrem <mr.efrem@gmail.com>
2016-11-04 02:19:21 +0400

    Many changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,37 @@
 'use strict';
 
 var webpack = require('webpack')
+var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 var env = process.env.NODE_ENV
+
+var reactExternal = {
+  root: 'React',
+  commonjs2: 'react',
+  commonjs: 'react',
+  amd: 'react'
+}
+
+var reduxExternal = {
+  root: 'Redux',
+  commonjs2: 'redux',
+  commonjs: 'redux',
+  amd: 'redux'
+}
+
+var reactReduxExternal = {
+  root: 'ReactRedux',
+  commonjs2: 'react-redux',
+  commonjs: 'react-redux',
+  amd: 'react-redux'
+}
+
 var config = {
+  externals: {
+    'react': reactExternal,
+    'redux': reduxExternal,
+    'react-redux': reactReduxExternal
+  },
   module: {
     loaders: [
       { test: /\.js$/, loaders: ['babel-loader'], exclude: /node_modules/ }
@@ -17,7 +45,8 @@ var config = {
     new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(env)
-    })
+    }),
+    /* new BundleAnalyzerPlugin() */
   ]
 };
 
@@ -29,13 +58,7 @@ if (env === 'production') {
         unsafe: true,
         unsafe_comps: true,
         warnings: false,
-        screw_ie8: false
-      },
-      mangle: {
-        screw_ie8: false
-      },
-      output: {
-        screw_ie8: false
+        screw_ie8: true
       }
     })
   )
